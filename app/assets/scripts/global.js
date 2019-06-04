8e20093de36ee866dddcb67e3d94216fde86acbf
Carlos Benavides <carlos.benavides@digitas.com>
2019-06-04 09:11:50 -0600

    new/clea sk project

---
+++ app/assets/scripts/global.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var APP = window.APP = window.APP || {};
+
+APP.global = (function(){
+
+    var initGlobalComponents = function() {
+
+    };
+
+    var initPageComponents = function() {
+        APP.core.controller.init();
+    };
+
+    var init = function() {
+
+        /**
+        * initialize global components
+        */
+        initGlobalComponents();
+
+        /**
+        * initialize components for the current page
+        */
+        initPageComponents();
+    };
+
+    /**
+    * interfaces to public functions
+    */
+    return {
+        init: init
+    };
+
+}());
+
+document.addEventListener("DOMContentLoaded", function() {
+  APP.global.init();
+});
